70ca067d65414811e556ad28754fe1a91f76949b
Aneesh M Gangadhar <aneesh.mg2007@gmail.com>
2017-09-19 22:01:48 +0530

    cleanup and created build script

--- build.js
+++ build.js
@@ -1,10 +1,14 @@
 const fs = require('fs');
-const imagemin = require('imagemin');
-const imageminJpegtran = require('imagemin-jpegtran');
-const imageminPngquant = require('imagemin-pngquant');
 const UglifyJS = require("uglify-js");
 const minify = require('html-minifier').minify;
 const CleanCSS = require('clean-css');
+
+
+let htmlFiles = ['index.html']
+let cssFiles = ['css/main.css']
+let jsFiles = ['js/main.js']
+
+
 
 
 const contents = fs.readFileSync('README.md', 'utf8');
